d2476edd75261943852ed90da6b3584402cd8335
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-08 16:17:51 +0200

    created fibonacci sequence

--- src/main/java/com/codecool/Fibonacci.java
+++ src/main/java/com/codecool/Fibonacci.java
@@ -1,4 +1,21 @@
 package com.codecool;
 
-public class Fibonacci {
+import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.Stream.*;
+
+public class Fibonacci implements Supplier{
+
+    int firstNumber = 0;
+    int secondNumber = 1;
+
+    @Override
+    public Object get() {
+        Integer nextFibonacciNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = nextFibonacciNumber;
+        return nextFibonacciNumber;
+    }
 }
